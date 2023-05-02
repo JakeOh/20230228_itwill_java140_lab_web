3bf17961e82d06df5be4728d53ed052c062fbc29
JakeOh <jake.oh.lecture@gmail.com>
2023-05-02 17:41:01 +0900

    servlet

--- jsp1/src/main/java/com/itwill/jsp1/FirstServlet.java
+++ jsp1/src/main/java/com/itwill/jsp1/FirstServlet.java
@@ -1,13 +1,13 @@
 package com.itwill.jsp1;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class FirstServlet
