e2c8de5cf19134240abd6cf923bd7f9f6e27800f
JakeOh <jake.oh.lecture@gmail.com>
2023-05-15 11:23:10 +0900

    encoding filter

--- jsp2/src/main/java/com/itwill/post/filter/EncodingFilter.java
+++ jsp2/src/main/java/com/itwill/post/filter/EncodingFilter.java
@@ -6,8 +6,9 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.io.IOException;
 
 import org.slf4j.Logger;
@@ -16,6 +17,9 @@ import org.slf4j.LoggerFactory;
 /**
  * Servlet Filter implementation class EncodingFilter
  */
+// Filter가 처리할 요청 주소(패턴) 설정:
+// (1) web.xml 파일에서 설정하거나
+// (2) @WebFilter 애너테이션으로 설정할 수 있음.
 public class EncodingFilter extends HttpFilter implements Filter {
 
     // Slf4j 로깅 기능:
@@ -45,6 +49,8 @@ public class EncodingFilter extends HttpFilter implements Filter {
 		log.info("doFilter() : chain.doFilter() 호출 전");
 
 		// 클라이언트에서 온 요청을 컨트롤러(서블릿)에게 전달하기 전에 실행할 코드들을 작성.
+		// 요청(request)의 인코딩 타입을 "UTF-8"로 설정:
+		request.setCharacterEncoding("UTF-8");
 
 		// 요청을 필터체인의 그 다음 단계로 전달 -> 컨트롤러 메서드가 호출됨.
 		chain.doFilter(request, response);
