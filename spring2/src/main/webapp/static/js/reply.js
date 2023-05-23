6074990e545d81f1f61f5886fc5bae34e96ff3da
JakeOh <jake.oh.lecture@gmail.com>
2023-05-23 12:17:54 +0900

    reply

---
+++ spring2/src/main/webapp/static/js/reply.js
@@ -0,0 +1,15 @@
+/**
+ * reply.js
+ * 댓글 등록, 목록 검색, 수정, 삭제
+ * /post/detail.jsp에 포함.
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // 댓글 등록/목록 보이기/숨기기 토글 버튼에 이벤트 리스너를 등록
+    const btnToggleReply = document.querySelector('button#btnToggleReply');
+    btnToggleReply.addEventListener('click', () => {
+        alert('토글 버튼');
+    });
+    
+});
