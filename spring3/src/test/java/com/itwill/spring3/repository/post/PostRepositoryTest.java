874d92ac60acaed915ec5757c5bca527c30b66af
JakeOh <jake.oh.lecture@gmail.com>
2023-06-27 12:11:32 +0900

    JUnit test: JPA, Hibernate, CRUD

--- spring3/src/test/java/com/itwill/spring3/repository/post/PostRepositoryTest.java
+++ spring3/src/test/java/com/itwill/spring3/repository/post/PostRepositoryTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.spring3.dto.PostUpdateDto;
 
@@ -49,7 +48,7 @@ public class PostRepositoryTest {
                 entity.getCreatedTime(), entity.getModifiedTime());
     }
     
-    @Test
+//    @Test
     public void update() {
         // 업데이트하기 전의 엔터티 검색:
         Post entity = postRepository.findById(1L).orElseThrow();
@@ -63,6 +62,22 @@ public class PostRepositoryTest {
         
         // 엔터티를 수정:
         entity.update(dto);
+        
+        // DB 테이블 업데이트:
+        // JPA에서는 insert와 update 메서드가 구분되어 있지 않음.
+        // save() 메서드의 아규먼트가 DB에 없는 엔터티이면 insert, DB에 있는 엔터티이면 update를 실행.
+        postRepository.saveAndFlush(entity);
+    }
+    
+    @Test
+    public void testDelete() {
+        long count = postRepository.count(); // DB 테이블의 행의 개수(엔터티 개수)
+        log.info("삭제 전 count = {}", count);
+        
+        postRepository.deleteById(42L);
+        
+        count = postRepository.count();
+        log.info("삭제 후 count = {}", count);
     }
     
 }
