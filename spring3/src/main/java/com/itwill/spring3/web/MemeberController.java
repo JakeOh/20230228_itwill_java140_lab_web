0b5d2aff67c256dbaf1de69565d04ebe55fdd9a0
JakeOh <jake.oh.lecture@gmail.com>
2023-07-03 15:21:42 +0900

    sign up & log in

--- spring3/src/main/java/com/itwill/spring3/web/MemeberController.java
+++ spring3/src/main/java/com/itwill/spring3/web/MemeberController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.spring3.dto.member.MemberSignUpDto;
+import com.itwill.spring3.service.MemberService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/member")
 public class MemeberController {
+    
+    private final MemberService memberService;
     
     @GetMapping("/signup")
     public void signUp() {
@@ -23,7 +28,9 @@ public class MemeberController {
     public String signUp(MemberSignUpDto dto) {
         log.info("signUp(dto={}) POST", dto);
         
-        // TODO: 회원 가입 서비스 호출
+        // 회원 가입 서비스 호출
+        Long id = memberService.registerMember(dto);
+        log.info("회원 가입 id = {}", id);
         
         // 회원 가입 이후에 로그인 화면으로 이동(redirect):
         return "redirect:/login";
